3ce14cef5209e0424844986f50d20be5e9d07b1a
mminnammi <ra0114fv@ed.ritsumei.ac.jp>
2024-12-09 12:28:09 +0900

    Add files via upload

--- mission_4-5.js
+++ mission_4-5.js
@@ -1,16 +1,18 @@
 function omikujishow(){
-	var message1 = "大吉";
-	var message2 = "中吉";
-	var message3 = "小吉";
+	var message1 = "大吉！！やったね！";
+	var message2 = "中吉！いいことあるかも";
+	var message3 = "小吉。今日も一日頑張ろう。";
 
 	var omikuji = new Array(message1,message2,message3);
+    var image = ['daikichi.jpg', 'tyuukichi.jpg', 'syoukichi.jpg'];
 		
 	var number1 = Math.random();
 	var number2 = number1 * 3;
 	var number = Math.floor(number2);
 	var message = omikuji[number];
+
+    main.src = image[number] ;
 	
-	var object = document.getElementById("message1");
-	message1.style.color = "red";
+	var object = document.getElementById("omikuji");
 	object.innerText = message;
 }
